66c11035bd354539e48ebf956e36a4c4e96d7aa7
Ameya Daigavane <ameya.d.98@gmail.com>
2020-09-05 20:33:26 +0530

    Add global params and variables for enforcing global constraints.

--- smt_network_model.py
+++ smt_network_model.py
@@ -25,9 +25,9 @@ class Sender:
 
     # Print all of the parameters we've added.
     def print_params(self):
-        print('Parameters:')
+        print('Sender %s Parameters:' % self.id)
         for param in sorted(self._params, key=lambda param: param.lower()):
-            print('- %s: %s' % (param, getattr(self, param)))
+            print(' - %s: %s' % (param, getattr(self, param)))
 
     # Add constraint variables which will be made available via the '.' operator:
     # Eg. If we pass in vars_dict={
@@ -43,19 +43,21 @@ class Sender:
 
     # Print all of the variables we've added.
     def print_vars(self, brief=False):
-        print('Variables:')
+        print('Sender %s Variables:' % self.id)
         for var in sorted(self._vars, key=lambda var: var.lower()):
             if brief:
-                print('- %s' % (var))
+                print(' - %s' % (var))
             else:
-                print('- %s: %s' % (var, getattr(self, var)))
+                print(' - %s: %s' % (var, getattr(self, var)))
 
 class AbstractSMTNetworkModel(ABC):
     def __init__(self):
         super().__init__()
         self._solver = Solver()
         self._senders = {}
-    
+        self._global_params = set()
+        self._global_vars = set()
+
     # Register a sender with the current model.
     def register_sender(self, sender):
         if sender.id in self._senders:
@@ -71,57 +73,118 @@ class AbstractSMTNetworkModel(ABC):
     def get_sender(self, sender_id):
         return self._senders[sender_id]
 
+    ## Parameters-related.
+    # Add global parameters.
+    def add_global_params(self, params_dict):
+        for param_name, param_val in params_dict.items():
+            if param_name.startswith('_'):
+                raise ValueError("Cannot overwrite internal variable!")
+            self._global_params.add(param_name)
+            setattr(self, param_name, param_val)
+
+    # Print global parameters that we've added.
+    def print_global_params(self):
+        print('Global Parameters:')
+        if len(self._global_params) == 0:
+            print(' (none)')
+
+        for param in sorted(self._global_params, key=lambda param: param.lower()):
+            print(' - %s: %s' % (param, getattr(self, param)))
+
     # Add parameters for a particular sender.
-    def add_params(self, params_dict, sender_id):
+    def add_params_for_sender(self, params_dict, sender_id):
         self._senders[sender_id].add_params(params_dict)
 
     # Print parameters for a particular sender.
-    def print_params(self, sender_id):
+    def print_params_for_sender(self, sender_id):
         self._senders[sender_id].print_params()
 
+    # Print ALL parameters.
+    def print_all_params(self):
+        for sender_id in self._senders:
+            self.print_params_for_sender(sender_id)
+        self.print_global_params()
+        print()
+
+    ## Constraint variables-related.
+    # Add global constraint variables.
+    def add_global_constraint_vars(self, vars_dict):
+        for var_name, var_val in vars_dict.items():
+            if var_name.startswith('_'):
+                raise ValueError("Cannot overwrite internal variable!")
+            self._global_vars.add(var_name)
+            setattr(self, var_name, var_val)
+
+    # Print global parameters that we've added.
+    def print_global_vars(self, brief=False):
+        print('Global Variables:')
+        if len(self._global_vars) == 0:
+            print(' (none)')
+
+        for var in sorted(self._global_vars, key=lambda var: var.lower()):
+            if brief:
+                print(' - %s' % (var))
+            else:
+                print(' - %s: %s' % (var, getattr(self, var)))
+
     # Add variables used in constraints for a particular sender.
-    def add_constraint_vars(self, vars_dict, sender_id):
+    def add_constraint_vars_for_sender(self, vars_dict, sender_id):
         self._senders[sender_id].add_constraint_vars(vars_dict)
 
-    # Print parameters for a particular sender.
-    def print_vars(self, sender_id, brief=False):
+    # Print variables for a particular sender.
+    def print_vars_for_sender(self, sender_id, brief=False):
         self._senders[sender_id].print_vars(brief=brief)
 
+    # Print ALL variables.
+    def print_all_vars(self, brief=False):
+        for sender_id in self._senders:
+            self.print_vars_for_sender(sender_id, brief=brief)
+        self.print_global_vars(brief=brief)
+        print()
+
+    ## Constraints-related.
     # Add a generic constraint.
     def add_constr(self, constr):
         self._solver.add(constr)
 
+    ## Sender-wise constraints.
     # Constraints on the flow itself.
     @abstractmethod
-    def add_flow_constraints(self, sender_id):
+    def add_flow_constraints_for_sender(self, sender_id):
         pass
     
     # Initial constraints on the variables.
     @abstractmethod
-    def add_initial_constraints(self, sender_id):
+    def add_initial_constraints_for_sender(self, sender_id):
         pass
 
     # Additional constraints on the solution (such as no loss, max queueing delay, and so on).
-    @abstractmethod
-    def add_additional_constraints(self, sender_id):
+    # This is optional.
+    def add_additional_constraints_for_sender(self, sender_id):
         pass
 
     # Constraints that define the congestion control algorithm.
     @abstractmethod
-    def add_congestion_control(self, sender_id):
+    def add_congestion_control_for_sender(self, sender_id):
         pass
     
     # Add ALL constraints for a sender!
     def add_all_constraints_for_sender(self, sender_id):
-        self.add_initial_constraints(sender_id)
-        self.add_flow_constraints(sender_id)
-        self.add_congestion_control(sender_id)
-        self.add_additional_constraints(sender_id)
+        self.add_initial_constraints_for_sender(sender_id)
+        self.add_flow_constraints_for_sender(sender_id)
+        self.add_congestion_control_for_sender(sender_id)
+        self.add_additional_constraints_for_sender(sender_id)
+
+    ## Global constraints.
+    # This is optional.
+    def add_global_constraints(self):
+        pass
 
     # Add ALL constraints for all senders!
     def add_all_constraints(self):
         for sender_id in self._senders:
             self.add_all_constraints_for_sender(sender_id)
+        self.add_global_constraints()
 
     # Check if all constraints are satisfied.
     def check(self):
