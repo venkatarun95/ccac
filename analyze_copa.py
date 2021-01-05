ee9dc51ecd19abcfb932cd1469b943480c4cd973
Venkat Arun <venkatarun95@gmail.com>
2021-01-05 11:38:05 -0500

    Improved Copa bounds

--- analyze_copa.py
+++ analyze_copa.py
@@ -11,23 +11,23 @@ def copa_steady_state(
     cfg: ModelConfig, err: float, timeout: float
 ):
     alpha_thresh = 0.1 * cfg.C * cfg.R
-    q_thresh = 5 * cfg.C * cfg.R
+    q_thresh = 4 * cfg.C * cfg.R + 2 * Real("alpha")
     cwnd_thresh = 0.9 * cfg.C * cfg.R
-    cwnd_thresh_u = 5 * cfg.C * cfg.R
+    cwnd_thresh_u = 4 * cfg.C * cfg.R + 2 * Real("alpha")
     T = cfg.T
 
     dur = freedom_duration(cfg)
 
-    # If queue < q_thresh and cwnd < cwnd_thresh, cwnd increases
+    # If queue < q_thresh and cwnd < cwnd_thresh, cwnd increases by at-least
+    # alpha / 2
     s = make_solver(cfg)
     conds = []
     for n in range(cfg.N):
-        for t in range(dur):
-            s.add(Real(f"cwnd_{n},{t}") <= cwnd_thresh)
-            conds.append(Real(f"cwnd_{n},{T-1-t}")
-                         < Real(f"cwnd_{n},{dur-1}") + Real("alpha") / 2)
+        s.add(Real(f"cwnd_{n},{dur-1}") <= cwnd_thresh)
+        conds.append(Real(f"cwnd_{n},{T-1}")
+                     < Real(f"cwnd_{n},{dur-1}") + Real("alpha") / 2)
     s.add(Or(*conds))
-    s.add(Real("tot_inp_0") <= q_thresh)
+    s.add(Real(f"tot_inp_{dur-1}") - Real(f"tot_out_{dur-1}") <= q_thresh)
     s.add(Real("alpha") < alpha_thresh)
     qres = run_query(s, cfg, timeout=timeout)
     print("Cwnd increases:", qres.satisfiable)
@@ -35,9 +35,8 @@ def copa_steady_state(
     # If queue < q_thresh and cwnd < cwnd_thresh, queue never exceeds q_thresh
     s = make_solver(cfg)
     for n in range(cfg.N):
-        for t in range(dur):
-            s.add(Real(f"cwnd_{n},{t}") <= cwnd_thresh)
-    s.add(Real("tot_inp_0") <= q_thresh)
+        s.add(Real(f"cwnd_{n},{dur-1}") <= cwnd_thresh)
+    s.add(Real(f"tot_inp_{dur-1}") - Real(f"tot_out_{dur-1}") <= q_thresh)
     s.add(Real(f"tot_inp_{T-1}") - Real(f"tot_out_{T-1}") > q_thresh)
     s.add(Real("alpha") < alpha_thresh)
     qres = run_query(s, cfg, timeout=timeout)
@@ -46,43 +45,41 @@ def copa_steady_state(
     # If Copa makes it to the steady state, it stays there
     s = make_solver(cfg)
     conds = []
-    for t in range(dur):
-        for n in range(cfg.N):
-            s.add(Real(f"cwnd_{n},{t}") >= cwnd_thresh)
-            s.add(Real(f"cwnd_{n},{t}") <= cwnd_thresh_u)
-            assert(t < T)
-            conds.append(Real(f"cwnd_{n},{T-1-t}") < cwnd_thresh)
-            conds.append(Real(f"cwnd_{n},{T-1-t}") > cwnd_thresh_u)
-        s.add(Real(f"tot_inp_{t}") - Real(f"tot_out_{t}") <= q_thresh)
+    for n in range(cfg.N):
+        s.add(Real(f"cwnd_{n},{dur-1}") <= cwnd_thresh_u)
+        s.add(Real(f"cwnd_{n},{dur-1}") >= cwnd_thresh)
+        conds.append(Real(f"cwnd_{n},{T-1}") < cwnd_thresh)
+        conds.append(Real(f"cwnd_{n},{T-1}") > cwnd_thresh_u)
         conds.append(
-            Real(f"tot_inp_{T-1-t}") - Real(f"tot_out_{T-1-t}") > q_thresh)
+            Real(f"tot_inp_{T-1}") - Real(f"tot_out_{T-1}") > q_thresh)
+    s.add(Real(f"tot_inp_{dur-1}") - Real(f"tot_out_{dur-1}") <= q_thresh)
     s.add(Real("alpha") < alpha_thresh)
     s.add(Or(*conds))
     qres = run_query(s, cfg, timeout=timeout)
     print("Stays there: ", qres.satisfiable)
 
-    # If queue > q_thresh and cwnd <= cwnd_thresh_u, queue will fall
+    # If queue > q_thresh and cwnd <= cwnd_thresh_u, queue will fall by
+    # at least alpha and cwnd never exceeds cwnd_thresh_u
     s = make_solver(cfg)
     conds = []
-    for t in range(dur):
-        for n in range(cfg.N):
-            s.add(Real(f"cwnd_{n},{t}") <= cwnd_thresh)
-        s.add(Real(f"tot_inp_{t}") > q_thresh)
-        conds.append(Real(f"tot_inp_{T-1-t}") - Real(f"tot_out_{T-1-t}")
-                     >= Real("tot_inp_0") - Real("alpha"))
+    for n in range(cfg.N):
+        s.add(Real(f"cwnd_{n},{dur-1}") <= cwnd_thresh_u)
+        conds.append(Real(f"cwnd_{n},{T-1}") > cwnd_thresh_u)
+    conds.append(Real(f"tot_inp_{T-1}") - Real(f"tot_out_{T-1}")
+                 >= Real("tot_inp_0") - Real("alpha"))
+    s.add(Real(f"tot_inp_{dur-1}") - Real(f"tot_out_{dur-1}") > q_thresh)
     s.add(Or(*conds))
     s.add(Real("alpha") < alpha_thresh)
     qres = run_query(s, cfg, timeout=timeout)
     print("Queue always falls", qres.satisfiable)
 
-    # If cwnd > cwnd_thresh_u, cwnd will fall
+    # If cwnd > cwnd_thresh_u, cwnd will fall by at-least alpha
     s = make_solver(cfg)
     conds = []
-    for t in range(dur):
-        for n in range(cfg.N):
-            s.add(Real(f"cwnd_{n},{t}") > cwnd_thresh_u)
-            conds.append(Real(f"cwnd_{n},{T-1-t}")
-                         >= Real(f"cwnd_{n},{dur-1}") - Real("alpha"))
+    for n in range(cfg.N):
+        s.add(Real(f"cwnd_{n},{dur-1}") > cwnd_thresh_u)
+        conds.append(Real(f"cwnd_{n},{T-1}")
+                     >= Real(f"cwnd_{n},{dur-1}") - Real("alpha"))
     s.add(Or(*conds))
     s.add(Real("alpha") < alpha_thresh)
     qres = run_query(s, cfg, timeout=timeout)
