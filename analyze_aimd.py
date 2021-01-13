71187aa96a70f0ca7616bcc1e1095d416a483b7f
Venkat Arun <venkatarun95@gmail.com>
2021-01-12 19:27:20 -0500

    Correct analyze_aimd + more efficient

--- analyze_aimd.py
+++ analyze_aimd.py
@@ -1,10 +1,12 @@
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
+from z3 import Real, Or
 
+from cache import run_query
 from model import find_cwnd_incr_bound, find_cwnd_stay_bound,\
     find_const_cwnd_util_lbound
-from multi_flow import ModelConfig
+from multi_flow import ModelConfig, make_solver, freedom_duration
 
 
 def single_flow_util(
@@ -14,7 +16,7 @@ def single_flow_util(
     there '''
 
     # Plot as a function of buffer size (droptail)
-    buf_sizes = np.linspace(0, 5, 10)
+    buf_sizes = np.linspace(0, 5, 10)[1:]
     cwnd_bounds = []
     util_bounds = []
 
@@ -23,18 +25,43 @@ def single_flow_util(
         cfg.buf_max = buf_size
         incr_bound = find_cwnd_incr_bound(cfg, None, err, timeout)
         print(f"For buffer size {buf_size} incr_bound={incr_bound}")
-        stay_bound = find_cwnd_stay_bound(cfg, None, err, timeout)
-        print(f"For buffer size {buf_size} stay_bound={stay_bound}")
+
+        # Same as find_cwnd_stay_bound
+        s = make_solver(cfg)
+        conds = []
+        dur = freedom_duration(cfg)
+        for n in range(cfg.N):
+            for t in range(dur):
+                s.add(Real(f"cwnd_{n},{t}") >= incr_bound[0])
+                # We need all the last freedom_duration(cfg) timesteps to be
+                # large so we can apply induction to extend theorem to infinity
+                conds.append(Real(f"cwnd_{n},{cfg.T-1-t}") < incr_bound[0])
+        s.add(Or(*conds))
+        print(f"Testing init cwnd for stay = {incr_bound[0]} BDP")
+        qres = run_query(s, cfg, timeout=timeout)
+        print(qres.satisfiable)
+
+        if qres.satisfiable != "unsat":
+            print("Could not prove bound. Will search for a new stay bound")
+            stay_bound = find_cwnd_stay_bound(cfg, None, err, timeout)
+            if stay_bound[0] > incr_bound[0]:
+                print("Failed to prove any bounds: ", stay_bound)
+                return
+            bound = stay_bound[0]
+        else:
+            # This is the definitive bound
+            bound = incr_bound[0]
 
         # This is a definitive bound
-        bound = min(incr_bound[0], stay_bound[0])
-        cwnd_thresh = bound * cfg.C * cfg.R
+        cwnd_thresh = bound
         util_bound = find_const_cwnd_util_lbound(
             cfg, cwnd_thresh, err, timeout)
         print(f"For buffer size {buf_size} util_bound={util_bound}")
 
         cwnd_bounds.append(cwnd_thresh)
         util_bounds.append(util_bound[0] * 100)
+
+    print([x for x in zip(buf_sizes, cwnd_bounds, util_bounds)])
 
     fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
     ax1.plot(buf_sizes, cwnd_bounds)
@@ -44,6 +71,7 @@ def single_flow_util(
     ax2.set_xlabel("Buffer size (in BDP)")
     plt.show()
     plt.savefig("/tmp/single_flow_util.svg")
+    print(buf_sizes, cwnd_bounds, util_bounds)
 
 
 if __name__ == "__main__":
