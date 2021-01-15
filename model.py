6e96d5324885663021732c2082dee6c64295017c
Venkat Arun <venkatarun95@gmail.com>
2021-01-15 11:17:08 -0500

    Refactored into find_bound + periodic_low_util

--- model.py
+++ model.py
@@ -1,11 +1,27 @@
 import argparse
 import pickle as pkl
-from typing import Optional, Tuple
-from z3 import And, Or, Real
+from typing import Callable, Optional, Tuple
+from z3 import And, Or, Real, Solver
 
 from binary_search import BinarySearch, sat_to_val
 from cache import QueryResult, run_query
 from multi_flow import ModelConfig, freedom_duration, make_solver, plot_model
+
+
+def find_bound(model_cons: Callable[[ModelConfig, float], Solver],
+               cfg: ModelConfig, search: BinarySearch, timeout: float):
+    while True:
+        thresh = search.next_pt()
+        if thresh is None:
+            break
+        s = model_cons(cfg, thresh)
+
+        print(f"Testing init cwnd for stay = {thresh}")
+        qres = run_query(s, cfg, timeout=timeout)
+
+        print(qres.satisfiable)
+        search.register_pt(thresh, sat_to_val(qres.satisfiable))
+    return search.get_bounds()
 
 
 def find_lower_tpt_bound(
@@ -133,17 +149,64 @@ def find_cwnd_incr_bound(
     return search.get_bounds()
 
 
-def find_cwnd_stay_bound(
-    cfg: ModelConfig, max_cwnd: Optional[float], err: float, timeout: float
+def cwnd_stay_bound(cfg: ModelConfig, thresh: float) -> Solver:
+    s = make_solver(cfg)
+    conds = []
+    dur = freedom_duration(cfg)
+    for n in range(cfg.N):
+        for t in range(dur):
+            s.add(Real(f"cwnd_{n},{t}") >= thresh)
+        for t in range(dur, cfg.T):
+            # We need all the last freedom_duration(cfg) timesteps to be
+            # large so we can apply induction to extend theorem to infinity
+            conds.append(Real(f"cwnd_{n},{t}") < thresh)
+    s.add(Or(*conds))
+    return s
+
+
+def find_periodic_low_util(
+    cfg: ModelConfig, no_loss: bool, err: float, timeout: float,
 ):
-    ''' Now find a cwnd such that if it starts above this value, it will remain
-    there '''
-    if max_cwnd is None:
-        if cfg.buf_max is None:
-            print("Error: Neither max_cwnd nor buf_max are specified")
-            return
-        max_cwnd = cfg.C * cfg.R + cfg.buf_max
-    search = BinarySearch(0.01, max_cwnd, err)
+    T = cfg.T
+
+    search = BinarySearch(0, 1, err)
+    while True:
+        util = search.next_pt()
+        if util is None:
+            break
+
+        s = make_solver(cfg)
+
+        # Make sure everything is periodic
+        dur = freedom_duration(cfg)
+        for n in range(cfg.N):
+            for t in range(dur):
+                tf = cfg.T-dur+t
+                s.add(Real(f"cwnd_{n},{t}") == Real(f"cwnd_{n},{tf}"))
+
+            s.add(Real(f"inp_{n},0") - Real(f"out_{n},0")
+                  == Real(f"inp_{n},{T-1}") - Real(f"out_{n},{T-1}"))
+            s.add(Real(f"losts_{n},0") - Real(f"loss_detected_{n},0")
+                  == Real(f"losts_{n},{T-1}") - Real(f"loss_detected_{n},{T-1}"))
+
+            if no_loss:
+                s.add(Real(f"losts_{n},0") == 0)
+        s.add(Real(f"tot_out_{cfg.T-1}") < util * cfg.C * (T - 1))
+
+        print(f"Testing {util * 100}% utilization")
+        qres = run_query(s, cfg, timeout=timeout)
+
+        print(qres.satisfiable)
+        search.register_pt(util, sat_to_val(qres.satisfiable))
+    return search.get_bounds()
+
+
+def find_periodic_low_cwnd(
+    cfg: ModelConfig, no_loss: bool, err: float, timeout: float
+):
+    T = cfg.T
+
+    search = BinarySearch(0, cfg.C * T + cfg.buf_max, err)
     while True:
         thresh = search.next_pt()
         if thresh is None:
@@ -151,17 +214,27 @@ def find_cwnd_stay_bound(
 
         s = make_solver(cfg)
 
-        conds = []
+        # Make sure everything is periodic
         dur = freedom_duration(cfg)
         for n in range(cfg.N):
             for t in range(dur):
-                s.add(Real(f"cwnd_{n},{t}") >= thresh)
-                # We need all the last freedom_duration(cfg) timesteps to be
-                # large so we can apply induction to extend theorem to infinity
-                conds.append(Real(f"cwnd_{n},{cfg.T-1-t}") < thresh)
-        s.add(Or(*conds))
+                tf = cfg.T-dur+t
+                s.add(Real(f"cwnd_{n},{t}") == Real(f"cwnd_{n},{tf}"))
 
-        print(f"Testing init cwnd for stay = {thresh}")
+            s.add(Real(f"inp_{n},0") - Real(f"losts_{n},0") - Real(f"out_{n},0")
+                  == Real(f"inp_{n},{T-1}") - Real(f"losts_{n},{T-1}") - Real(f"out_{n},{T-1}"))
+            s.add(Real(f"losts_{n},0") - Real(f"loss_detected_{n},0")
+                  == Real(f"losts_{n},{T-1}") - Real(f"loss_detected_{n},{T-1}"))
+
+            if no_loss:
+                s.add(Real(f"losts_{n},0") == 0)
+
+            s.add(Or(*[Real(f"cwnd_{n},{t}") < thresh for t in range(T)]))
+
+        s.add(Real("tot_out_0") - (-Real("wasted_0"))
+              == Real(f"tot_out_{T-1}") - (cfg.C * (T-1) - Real(f"wasted_{T-1}")))
+
+        print(f"Testing cwnd thresh {thresh / (cfg.C * cfg.R)} BDP")
         qres = run_query(s, cfg, timeout=timeout)
 
         print(qres.satisfiable)
@@ -201,6 +274,18 @@ if __name__ == "__main__":
     const_cwnd_util_lbound_args.add_argument(
         "--cwnd-thresh", type=float, required=True)
 
+    periodic_low_util_args = subparsers.add_parser(
+        "periodic_low_util",
+        parents=[cfg_args, common_args])
+    periodic_low_util_args.add_argument("--no-loss", action="store_const",
+                                        const=True, default=False)
+
+    periodic_low_cwnd_args = subparsers.add_parser(
+        "periodic_low_cwnd",
+        parents=[cfg_args, common_args])
+    periodic_low_cwnd_args.add_argument("--no-loss", action="store_const",
+                                        const=True, default=False)
+
     plot_args = subparsers.add_parser("plot")
     plot_args.add_argument("cache_file_name")
 
@@ -217,12 +302,21 @@ if __name__ == "__main__":
             cfg, args.max_cwnd, args.err, args.timeout)
         print(bounds)
     elif args.subcommand == "cwnd_stay_bound":
-        bounds = find_cwnd_stay_bound(
-            cfg, args.max_cwnd, args.err, args.timeout)
+        bounds = find_bound(cwnd_stay_bound, cfg,
+                            BinarySearch(0, cfg.C*cfg.R+cfg.buf_max, args.err),
+                            args.timeout)
         print(bounds)
     elif args.subcommand == "const_cwnd_util_lbound":
         bounds = find_const_cwnd_util_lbound(
             cfg, args.cwnd_thresh, args.err, args.timeout)
+        print(bounds)
+    elif args.subcommand == "periodic_low_util":
+        bounds = find_periodic_low_util(
+            cfg, args.no_loss, args.err, args.timeout)
+        print(bounds)
+    elif args.subcommand == "periodic_low_cwnd":
+        bounds = find_periodic_low_cwnd(
+            cfg, args.no_loss, args.err, args.timeout)
         print(bounds)
     elif args.subcommand == "plot":
         try:
@@ -233,5 +327,6 @@ if __name__ == "__main__":
             print(e)
         print(qres.satisfiable)
         if qres.satisfiable == "sat":
+            print(qres)
             assert(qres.model is not None)
             plot_model(qres.model, qres.cfg)
