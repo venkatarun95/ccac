211634e1f8a9c1aa5552e5a963bc5b721f618997
Venkat Arun <venkatarun95@gmail.com>
2021-04-07 11:20:23 -0700

    Implemented constant folding for loops and variable declarations and contexts

--- compiler/src/ast.rs
+++ compiler/src/ast.rs
@@ -6,6 +6,7 @@ pub enum DataType {
     Bool,
     Int,
     Real,
+    Void,
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
@@ -35,14 +36,14 @@ pub enum Cmp {
     Eq,
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub enum BoolOp {
     And(Vec<Node>),
     Or(Vec<Node>),
-    Not(Vec<Node>),
+    Not(Box<Node>),
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub enum Node {
     Constant {
         val: Constant,
@@ -72,17 +73,31 @@ pub enum Node {
         lower: Box<Node>,
         upper: Box<Node>,
     },
+    Stmts {
+        stmts: Vec<Node>,
+    },
     /// Loop over lo, lo+step, lo+2*step as long as the indices are <
     /// hi
     ForLoop {
+        varname: String,
         lo: Box<Node>,
         hi: Box<Node>,
         step: Box<Node>,
+        body: Box<Node>,
+    },
+    /// Pushes a new context for its children
+    Context {
+        child: Box<Node>,
+    },
+    /// Declare a new variable
+    VarDecl {
+        varname: String,
+        sym: Symbol,
     },
 }
 
 impl Node {
-    pub fn data_type(&self, ctx: &Context) -> DataType {
+    pub fn data_type(&self, ctx: &mut Context) -> DataType {
         match self {
             Self::Constant { val } => val.data_type(),
             Self::Variable { name, .. } => {
@@ -118,17 +133,53 @@ impl Node {
                 assert!(lower.data_type(ctx) == upper.data_type(ctx));
                 lower.data_type(ctx)
             }
-            Self::ForLoop { lo, hi, step } => {
-                if lo.data_type(ctx) != hi.data_type(ctx)
-                    || lo.data_type(ctx) != step.data_type(ctx)
-                {
-                    panic!("All parts of the for loop must have the same datatype. We have lo={:?}, hi={:?}, step={:?}", lo, hi, step);
+            Self::Stmts { stmts } => {
+                // Perform checks
+                for stmt in stmts {
+                    let dt = stmt.data_type(ctx);
+                    if dt != DataType::Bool && dt != DataType::Void {
+                        panic!(
+                            "Warning: Statement returns type {:?}, which does nothing.",
+                            dt
+                        );
+                    }
                 }
-                if lo.data_type(ctx) == DataType::Bool {
-                    panic!("For loop indices cannot have type bool");
-                }
-                lo.data_type(ctx)
+                DataType::Void
             }
+            Self::ForLoop { lo, hi, step, .. } => {
+                let dt = lo.data_type(ctx);
+                // Perform some checks
+                if dt != hi.data_type(ctx) || dt != step.data_type(ctx) {
+                    panic!("All parts of the for loop must have the same datatype. We have lo={:?}, hi={:?}, step={:?}", dt, hi.data_type(ctx), step.data_type(ctx));
+                }
+                if dt != DataType::Int && dt != DataType::Real {
+                    panic!("For loop indices cannot have type {:?}", lo.data_type(ctx));
+                }
+                DataType::Void
+            }
+            Self::Context { child, .. } => {
+                ctx.push();
+                let res = child.data_type(ctx);
+                ctx.pop();
+                res
+            }
+            Self::VarDecl { .. } => DataType::Void,
+        }
+    }
+
+    pub fn is_constant(&self) -> bool {
+        if let Self::Constant { .. } = self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn get_constant(&self) -> Option<Constant> {
+        if let Self::Constant { val } = self {
+            Some(*val)
+        } else {
+            None
         }
     }
 
@@ -151,16 +202,58 @@ impl Node {
 
     /// Recursively fold all constants and force to zero in
     /// multiplications
-    pub fn fold_const(&mut self, ctx: &Context) -> bool {
-        fn to_rational(v: Constant) -> Ratio<i64> {
+    pub fn fold_const(&mut self, ctx: &mut Context) -> bool {
+        fn to_rational(v: &Constant) -> Ratio<i64> {
             match v {
                 Constant::Bool(_) => panic!("Unexpected bool constant"),
-                Constant::Int(v) => Ratio::new(v, 1),
-                Constant::Real(v) => v,
+                Constant::Int(v) => Ratio::new(*v, 1),
+                Constant::Real(v) => *v,
+            }
+        }
+        fn to_bool(x: &Constant) -> bool {
+            if let Constant::Bool(x) = x {
+                *x
+            } else {
+                panic!("Expected bool in boolean operation, found {:?}", x)
             }
         }
 
+        // Delete constant values and return them in a separate vector. Also returns whether something changed
+        fn fold_vec(v: &mut Vec<Node>, ctx: &mut Context) -> (Vec<Constant>, bool) {
+            let mut consts = Vec::new();
+            let mut changed = false;
+            for t in v.iter_mut() {
+                changed |= t.fold_const(ctx);
+                if let Node::Constant { val, .. } = t {
+                    consts.push(*val);
+                }
+            }
+            if consts.len() > 1 {
+                // Remove all constant terms and add only the one constant
+                changed = true;
+                v.retain(|x| {
+                    if let Node::Constant { .. } = *x {
+                        false
+                    } else {
+                        true
+                    }
+                });
+                let data_type = v[0].data_type(ctx);
+                for x in v[1..].iter() {
+                    if x.data_type(ctx) != data_type {
+                        panic!(
+                            "All datatypes must be the same. Expected {:?} found {:?}",
+                            data_type,
+                            x.data_type(ctx)
+                        );
+                    }
+                }
+            }
+            (consts, changed)
+        }
+
         match self {
+            Self::Constant { .. } => false,
             Self::Variable { name, indices } => {
                 let mut change = false;
                 for i in indices {
@@ -182,8 +275,8 @@ impl Node {
                 let changed = left.fold_const(ctx) | right.fold_const(ctx);
                 if let Self::Constant { val: left } = **left {
                     if let Self::Constant { val: right } = **right {
-                        let left = to_rational(left);
-                        let right = to_rational(right);
+                        let left = to_rational(&left);
+                        let right = to_rational(&right);
                         let res = match cmp {
                             Cmp::Lt => left < right,
                             Cmp::Le => left <= right,
@@ -197,100 +290,179 @@ impl Node {
                 }
                 changed
             }
-            Self::Add { terms } => {
-                let mut changed = false;
-                let mut c_term = Ratio::<i64>::new(0, 1);
-                let mut num_const = 0;
-                for t in terms.iter_mut() {
-                    changed |= t.fold_const(ctx);
-                    if let Self::Constant { val, .. } = t {
-                        c_term += to_rational(*val);
-                        num_const += 1;
+            Self::BoolOp { op } => {
+                match op {
+                    BoolOp::And(v) => {
+                        let (consts, changed) = fold_vec(v, ctx);
+                        if !consts.iter().all(to_bool) {
+                            // The entire thing is false
+                            *self = Node::Constant {
+                                val: Constant::Bool(false),
+                            };
+                        }
+                        changed
+                    }
+                    BoolOp::Or(v) => {
+                        let (consts, changed) = fold_vec(v, ctx);
+                        if consts.iter().any(to_bool) {
+                            // The entire thing is false
+                            *self = Node::Constant {
+                                val: Constant::Bool(true),
+                            };
+                        }
+                        changed
+                    }
+                    BoolOp::Not(x) => {
+                        if let Node::Constant { val } = **x {
+                            if let Constant::Bool(val) = val {
+                                *self = Node::Constant {
+                                    val: Constant::Bool(!val),
+                                };
+                            } else {
+                                panic!(
+                                    "Cannot compute NOT of value of non-bool type {:?}",
+                                    val.data_type()
+                                );
+                            }
+                            true
+                        } else {
+                            false
+                        }
                     }
                 }
-
-                if num_const > 1 {
-                    // Remove all constant terms and add only the one constant
-                    terms.retain(|x| {
-                        if let Self::Constant { .. } = *x {
-                            false
-                        } else {
-                            true
+            }
+            Self::Add { terms } => {
+                let (consts, changed) = fold_vec(terms, ctx);
+                let c_term: Ratio<i64> = consts.iter().map(to_rational).sum();
+                if consts.len() > 0 {
+                    let val = match consts[0].data_type() {
+                        DataType::Real => Constant::Real(c_term),
+                        DataType::Int => {
+                            assert_eq!(*c_term.denom(), 1);
+                            Constant::Int(*c_term.numer())
                         }
-                    });
-                    changed = true;
-                    let data_type = terms[0].data_type(ctx);
-                    terms.push(Self::Constant {
-                        val: match data_type {
-                            DataType::Bool => panic!("Error in the compiler. Cannot add bools"),
-                            DataType::Int => {
-                                if *c_term.denom() != 1 {
-                                    panic!("Attempted to add Real to Int");
-                                }
-                                Constant::Int(*c_term.numer())
-                            }
-                            DataType::Real => Constant::Real(c_term),
-                        },
-                    });
+                        _ => unreachable!(),
+                    };
+                    terms.push(Node::Constant { val });
                 }
 
                 changed
             }
             Self::Mul { terms } => {
-                let mut changed = false;
-                let mut c_term = Ratio::<i64>::new(1, 1);
-                let mut num_const = 0;
-                for t in terms.iter_mut() {
-                    changed |= t.fold_const(ctx);
-                    if let Self::Constant { val, .. } = t {
-                        c_term *= to_rational(*val);
-                        num_const += 1;
-                    }
-                }
-
-                // Multiplication by zero. The entire thing is 0
-                if c_term == Ratio::new(0, 1) && terms.len() > 1 {
-                    *self = Self::Constant {
-                        val: match self.data_type(ctx) {
-                            DataType::Bool => panic!("Error in the compiler. Cannot add bools"),
-                            DataType::Int => Constant::Int(0),
-                            DataType::Real => Constant::Real(c_term),
-                        },
-                    };
-                    return true;
-                }
-
-                if num_const > 1 {
-                    // Remove all constant terms and add only the one constant
-                    terms.retain(|x| {
-                        if let Self::Constant { .. } = *x {
-                            false
-                        } else {
-                            true
+                let (consts, changed) = fold_vec(terms, ctx);
+                let c_term: Ratio<i64> = consts.iter().map(to_rational).product();
+                if consts.len() > 0 {
+                    let val = match consts[0].data_type() {
+                        DataType::Real => Constant::Real(c_term),
+                        DataType::Int => {
+                            assert_eq!(*c_term.denom(), 1);
+                            Constant::Int(*c_term.numer())
                         }
-                    });
-                    changed = true;
-                    let data_type = terms[0].data_type(ctx);
-                    terms.push(Self::Constant {
-                        val: match data_type {
-                            DataType::Bool => {
-                                panic!("Error in the compiler. Cannot multiply bools")
-                            }
-                            DataType::Int => {
-                                if *c_term.denom() != 1 {
-                                    panic!("Attempted to add Real to Int");
-                                }
-                                Constant::Int(*c_term.numer())
-                            }
-                            DataType::Real => Constant::Real(c_term),
-                        },
-                    });
+                        _ => unreachable!(),
+                    };
+                    // If multiplying by zero, the entire thing will be 0
+                    if *c_term.numer() == 0 {
+                        *self = Node::Constant { val };
+                        return true;
+                    }
+                    terms.push(Node::Constant { val });
                 }
 
                 changed
             }
             Self::Range { lower, upper, .. } => lower.fold_const(ctx) | upper.fold_const(ctx),
-            _ => false,
+            Self::Stmts { stmts, .. } => {
+                let mut change = false;
+                for stmt in stmts {
+                    change |= stmt.fold_const(ctx);
+                }
+                change
+            }
+            Self::ForLoop {
+                varname,
+                lo,
+                hi,
+                step,
+                body,
+                ..
+            } => {
+                let change = lo.fold_const(ctx) || hi.fold_const(ctx) || step.fold_const(ctx);
+                if lo.is_constant() && hi.is_constant() && step.is_constant() {
+                    // Ensure the loop variable is declared and is not an array
+                    let loop_var = if let Some(var) = ctx.get(varname) {
+                        if var.data_type == DataType::Bool {
+                            panic!("Cannot loop over bools");
+                        }
+                        if var.indices.len() > 0 {
+                            panic!("Loop variables cannot be array elements");
+                        }
+                        var
+                    } else {
+                        panic!("Trying to use undeclared variable {} in loop", varname);
+                    };
+                    // Unroll the loop
+                    let lo = lo.get_constant().unwrap();
+                    let hi = hi.get_constant().unwrap();
+                    let step = step.get_constant().unwrap();
+
+                    // Generate the list of symbols generated by the for loop
+                    let mut cur = to_rational(&lo);
+                    let mut symbols = Vec::new();
+                    while cur < to_rational(&hi) {
+                        let sym = match loop_var.data_type {
+                            DataType::Bool => panic!("For loop cannot operate over bools"),
+                            DataType::Void => panic!("For loop cannot operate over voids"),
+                            DataType::Int => {
+                                if *cur.denom() != 1 {
+                                    panic!("Tried to use non-integer value '{}' for an integer loop variable '{}'", cur, varname);
+                                }
+                                Symbol {
+                                    data_type: DataType::Int,
+                                    val: Some(Constant::Int(*cur.numer())),
+                                    indices: vec![],
+                                }
+                            }
+                            DataType::Real => Symbol {
+                                data_type: DataType::Real,
+                                val: Some(Constant::Real(cur)),
+                                indices: vec![],
+                            },
+                        };
+                        symbols.push(sym);
+                        cur += to_rational(&step);
+                    }
+
+                    // Now unroll the loop for each of the generated symbols
+                    // The new node we'll replace `self` by
+                    let mut new_stmts = Vec::new();
+                    for sym in symbols {
+                        new_stmts.push(Self::Context {
+                            child: Box::new(Self::Stmts {
+                                stmts: vec![
+                                    Node::VarDecl {
+                                        varname: varname.clone(),
+                                        sym,
+                                    },
+                                    *body.clone(),
+                                ],
+                            }),
+                        });
+                    }
+
+                    return true;
+                }
+                change
+            }
+            Self::Context { child, .. } => {
+                ctx.push();
+                let res = child.fold_const(ctx);
+                ctx.pop();
+                res
+            }
+            Self::VarDecl { varname, sym } => {
+                ctx.insert(varname.clone(), sym.clone());
+                false
+            }
         }
     }
 }
--- compiler/src/context.rs
+++ compiler/src/context.rs
@@ -1,6 +1,7 @@
 use crate::ast::{Constant, DataType};
 use std::collections::HashMap;
 
+#[derive(Clone, Debug)]
 pub struct Symbol {
     pub data_type: DataType,
     /// Populated if the value is known at compile time. Note, arrays
@@ -10,4 +11,43 @@ pub struct Symbol {
     pub indices: Vec<usize>,
 }
 
-pub type Context = HashMap<String, Symbol>;
+#[derive(Debug)]
+pub struct Context {
+    /// Stack of contexts. Guaranteed to have at least one context
+    stack: Vec<HashMap<String, Symbol>>,
+}
+
+impl Context {
+    pub fn new() -> Self {
+        Self {
+            stack: vec![HashMap::new()],
+        }
+    }
+
+    pub fn insert(&mut self, name: String, sym: Symbol) {
+        if self.get(&name).is_some() {
+            panic!("Variable '{}' already declared", name);
+        }
+        self.stack.last_mut().as_mut().unwrap().insert(name, sym);
+    }
+
+    pub fn get<'a>(&'a self, name: &str) -> Option<&'a Symbol> {
+        for i in 0..self.stack.len() {
+            let i = self.stack.len() - 1 - i;
+            if let Some(s) = self.stack[i].get(name) {
+                return Some(s);
+            }
+        }
+        return None;
+    }
+
+    /// Push a new context in
+    pub fn push(&mut self) {
+        self.stack.push(HashMap::new());
+    }
+
+    /// Remove the last context
+    pub fn pop(&mut self) {
+        self.stack.pop();
+    }
+}
