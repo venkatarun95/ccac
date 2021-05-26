a5b77d56eb2856b43186e719bed0019fbe7deed7
Venkat Arun <venkatarun95@gmail.com>
2021-05-25 19:51:43 -0700

    Fixed copa proof

--- copa_proofs.py
+++ copa_proofs.py
@@ -1,4 +1,4 @@
-from z3 import Or
+from z3 import And, Or
 
 from cache import run_query
 from model import make_solver
@@ -16,28 +16,32 @@ def prove_steady_state(timeout=10):
     c.compose = True
     c.calculate_qdel = True
 
-    # The last cwnd value that is chosen completely freely
+    # The last cwnd value that is chosen completely freely. We'll treat this as
+    # the initial cwnd
     dur = c.R + c.D - 1
 
     # If cwnd > 4 BDP + alpha, cwnd wil decrease by at-least alpha
     s, v = make_solver(c)
+    # We are looking at infinite buffer, no loss case here and in the paper
+    s.add(And(v.L[0] == 0, v.L[-1] == 0))
+    s.add(v.alpha < (1 / 3) * c.C * c.R)
     s.add(v.c_f[0][dur] > 4*c.C*c.R + v.alpha)
     s.add(v.c_f[0][-1] >= v.c_f[0][dur] - v.alpha)
-    s.add(v.L[0] == 0)
-    s.add(v.alpha < (1 / 3) * c.C * c.R)
     print("Proving that cwnd will decrease when it is too big")
     qres = run_query(s, c, timeout)
     print(qres.satisfiable)
     assert(qres.satisfiable == "unsat")
 
     # If queue length is > 4 BDP + 2 alpha and cwnd < 4 BDP + alpha, queue
-    # length decreases by at-least alpha
+    # length decreases by at-least alpha and cwnd will not increase its bound
     s, v = make_solver(c)
+    s.add(And(v.L[0] == 0, v.L[-1] == 0))
+    s.add(v.alpha < (1 / 5) * c.C * c.R)
     s.add(v.c_f[0][dur] <= 4*c.C*c.R + v.alpha)
     s.add(v.A[0] - v.S[0] > 4*c.C*c.R + 2*v.alpha)
-    s.add(v.A[-1] - v.S[-1] > v.A[0] - v.S[0] - v.alpha)
-    s.add(v.L[0] == 0)
-    s.add(v.alpha < (1 / 5) * c.C * c.R)
+    s.add(Or(
+        v.A[-1] - v.S[-1] > v.A[0] - v.S[0] - v.alpha,
+        v.c_f[0][-1] > 4*c.C*c.R + v.alpha))
     print("Proving that if queue is too big and cwnd is small enough, then "
           "queue will fall")
     qres = run_query(s, c, timeout)
@@ -45,16 +49,18 @@ def prove_steady_state(timeout=10):
     assert(qres.satisfiable == "unsat")
 
     # If cwnd < BDP - alpha and queue length < 4 BDP + 2 alpha, cwnd increases
-    # by at-least alpha
+    # by at-least alpha and queue length does not increase its bound
     c.T = 15
     c.compose = False  # we definitely need it to prove cwnd increases
     s, v = make_solver(c)
+    s.add(And(v.L[0] == 0, v.L[-1] == 0))
+    s.add(v.alpha < (1 / 4) * c.C * c.R)
     s.add(v.c_f[0][dur] < c.C*c.R - v.alpha)
     s.add(v.A[0] - v.S[0] <= 4*c.C*c.R + 2*v.alpha)
     s.add(v.c_f[0][-1] < v.c_f[0][dur] + v.alpha)
-    s.add(v.c_f[0][-1] < c.C*c.R - v.alpha)
-    s.add(v.L[0] == 0)
-    s.add(v.alpha < (1 / 4) * c.C * c.R)
+    s.add(Or(
+        v.c_f[0][-1] < c.C*c.R - v.alpha,
+        v.A[-1] - v.S[-1] <= 4*c.C*c.R + 2*v.alpha))
     print("Proving that if cwnd is too small and the queue is small enough, "
           "cwnd increases")
     qres = run_query(s, c, timeout)
@@ -66,6 +72,8 @@ def prove_steady_state(timeout=10):
     c.compose = False
     s, v = make_solver(c)
     ors = []
+    s.add(v.alpha < (1 / 7) * c.C * c.R)
+    s.add(And(v.L[0] == 0, v.L[-1] == 0))
     s.add(v.c_f[0][dur] >= c.C*c.R - v.alpha)
     ors.append(v.c_f[0][-1] < c.C*c.R - v.alpha)
     s.add(v.c_f[0][dur] <= 4*c.C*c.R + 2*v.alpha)
@@ -73,8 +81,6 @@ def prove_steady_state(timeout=10):
     s.add(v.A[0] - v.S[0] <= 4*c.C*c.R + 2*v.alpha)
     ors.append(v.A[-1] - v.S[-1] > 4*c.C*c.R + 2*v.alpha)
 
-    s.add(v.alpha < (1 / 7) * c.C * c.R)
-    s.add(v.L[0] == 0)
     s.add(Or(*ors))
     print("Proving that if Copa has entered steady state, it will "
           "remain there")
