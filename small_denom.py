e868351a5ac1c72ab4532ade3130c15306927964
Anup Agarwal <anupa@andrew.cmu.edu>
2022-05-09 11:38:31 -0400

    Add small denominator solution

---
+++ small_denom.py
@@ -0,0 +1,90 @@
+''' Produce solutions with small denominators '''
+
+from fractions import Fraction
+import math
+from typing import Optional
+from z3 import If, Or, Real
+
+from .binary_search import BinarySearch
+from .cache import ModelDict, model_to_dict
+from .my_solver import MySolver
+
+def find_small_denom_soln(s: MySolver, max_denom: int) -> Optional[ModelDict]:
+    '''Find a solution that tries to maximize the number of variables that have a
+    demoninator smaller than `max_denom`
+    '''
+
+    sat = str(s.check())
+    if sat != "sat":
+        return None
+
+    m = model_to_dict(s.model())
+
+    best_m = m
+    best_obj = 0
+
+    # Number of variables that newly have a small denominator
+    objective = 0
+    max_objective = 0
+    old_obj = 0
+    for vname in m:
+        if type(m[vname]) is Fraction:
+            val = m[vname]
+            assert isinstance(val, Fraction)
+            if val.denominator > max_denom:
+                # Fractions just above and below the value
+                num = math.ceil(Fraction(val.numerator * max_denom,
+                                         val.denominator))
+                hi = Fraction(num, max_denom)
+                lo = Fraction(num - 1, max_denom)
+                assert lo < val and val < hi, f"Error in computing hi={hi} and lo={lo} for {val}"
+
+                s.add(Or(
+                    Real(vname) == val, Real(vname) == lo, Real(vname) == hi))
+
+                objective += If(Real(vname) == val, 0, 1)
+                max_objective += 1
+            else:
+                s.add(Real(vname) == val)
+                old_obj += 1
+
+
+    search = BinarySearch(0, max_objective, 1)
+    while True:
+        pt = search.next_pt()
+        if pt is None:
+            break
+
+        # Round to integer
+        pt_int = round(pt)
+
+        s.push()
+        s.add(objective >= pt_int)
+        sat = str(s.check())
+
+        if sat == "sat":
+            search.register_pt(pt, 1)
+            if pt_int > best_obj:
+                best_m = model_to_dict(s.model())
+                best_obj = pt_int
+
+        elif sat == "unknown":
+            search.register_pt(pt, 2)
+        else:
+            assert sat == "unsat", f"Unknown value: {sat}"
+            search.register_pt(pt, 3)
+
+        s.pop()
+
+    search.get_bounds()
+
+    new_obj = 0
+    for vname in best_m:
+        if type(best_m[vname]) is Fraction:
+            val = best_m[vname]
+            assert isinstance(val, Fraction)
+            if val.denominator <= max_denom:
+                new_obj += 1
+    print(f"Improved number of small numbers from {old_obj} to {new_obj} out of a max of {old_obj + max_objective}")
+
+    return best_m
